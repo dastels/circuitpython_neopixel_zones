b8ec99bcad88f1eb4552f51cc1df232f7cbe7c27
Dave Astels <dastels@daveastels.com>
2020-03-27 10:23:06 -0400

    Initial code checkin

---
+++ code.py
@@ -0,0 +1,61 @@
+import time
+from random import randint
+import digitalio
+import board
+import neopixel
+from neopixel_zones import Zone, ZoneCollection
+
+enable = digitalio.DigitalInOut(board.D10)
+enable.direction = digitalio.Direction.OUTPUT
+enable.value = True
+
+strip = neopixel.NeoPixel(board.D5, 60, brightness=1, auto_write=False)
+
+collection = ZoneCollection(strip)
+
+zone_1 = collection.add_zone(0, 20)
+zone_2 = collection.add_zone_with_size(20, 20)
+zone_3 = collection.add_zone(40, 60)
+
+for _ in range(10):
+    zone_1.fill((randint(128, 255), randint(0, 255), randint(0, 255)))
+    zone_2.fill((randint(0, 255), randint(128, 255), randint(0, 255)))
+    zone_3.fill((randint(0, 255), randint(0, 255), randint(128, 255)))
+    collection.show()
+    time.sleep(0.5)
+
+for _ in range(4):
+    for i in range(1, 20):
+        strip.fill((0x00, 0x00, 0x00))
+        zone_1[i] = (0xFF, 0x00, 0x00)
+        zone_2[i] = (0x00, 0xFF, 0x00)
+        zone_3[i] = (0x00, 0x00, 0xFF)
+        collection.show()
+        time.sleep(0.2)
+    for i in range(18, -1, -1):
+        strip.fill((0x00, 0x00, 0x00))
+        zone_1[i] = (0xFF, 0x00, 0x00)
+        zone_2[i] = (0x00, 0xFF, 0x00)
+        zone_3[i] = (0x00, 0x00, 0xFF)
+        collection.show()
+        time.sleep(0.2)
+
+for _ in range(4):
+    for i in range(20):
+        strip.fill((0x00, 0x00, 0x00))
+        start = i
+        end = min(i + 5, 20)
+        size = end - start
+        zone_1[start:end] = [(0xFF, 0x00, 0x00)] * size
+        zone_2[start:end] = [(0x00, 0xFF, 0x00)] * size
+        zone_3[start:end] = [(0x00, 0x00, 0xFF)] * size
+        collection.show()
+        time.sleep(0.2)
+
+while True:
+    strip.fill((0, 0, 0))
+    zone_1[randint(0, 19)] = (randint(0, 255), randint(0, 255), randint(0, 255))
+    zone_2[randint(0, 19)] = (randint(0, 255), randint(0, 255), randint(0, 255))
+    zone_3[randint(0, 19)] = (randint(0, 255), randint(0, 255), randint(0, 255))
+    collection.show()
+    time.sleep(0.1)
---
+++ neopixel_zones.py
@@ -0,0 +1,108 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Dave Astels
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Neopixel zone support
+--------------------------------------------------------------------------------
+* Author: Dave Astels
+"""
+
+from neopixel import NeoPixel
+
+
+class ZoneCollection:
+    """Manage a collection of zones, projecting them onto a physical collection of NeoPixels."""
+
+    def __init__(self, pixels):
+        """Create a zone collection on top of a NeoPixel instance.
+
+        :param neopixel.NeoPixel strip: the physical NeoPixel strip backing the collection
+        """
+        self._pixels = pixels
+        self._zones = []
+
+    def _add(self, zone):
+        self._zones.append(zone)
+        zone.collection = self
+        return zone
+
+    def add_zone(self, first, last_plus_one):
+        #        if first < 0 or last > self._pixels.
+        return self._add(Zone(first, last_plus_one))
+
+    def add_zone_with_size(self, first, size):
+        return self.add_zone(first, first + size)
+
+    @property
+    def brightness(self):
+        """Overall brightness of the pixel"""
+        return self._pixels.brightness
+
+    @brightness.setter
+    def brightness(self, brightness):
+        self._pixels.brightness = brightness
+
+    def __setitem__(self, index, val):
+        self._pixels[index] = val
+
+    def __getitem__(self, index):
+        return self._pixels[index]
+
+    def show(self):
+        self._pixels.show()
+
+
+class Zone:
+    def __init__(self, first, last_plus_one):
+        self._first = first
+        self._last_plus_one = last_plus_one
+
+    @property
+    def collection(self):
+        return self._collection
+
+    @collection.setter
+    def collection(self, coll):
+        self._collection = coll
+
+    def __setitem__(self, index, val):
+        if isinstance(index, slice):
+            self._collection[
+                (index.start + self._first) : (index.stop + self._first) : (index.step)
+            ] = val
+        else:
+            self._collection[index + self._first] = val
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return self._collection[
+                (index.start + self._first) : (index.stop + self._first) : (index.step)
+            ]
+        else:
+            return self._collection[index + self._first]
+
+    def fill(self, color):
+        for i in range(self._last_plus_one - self._first):
+            self[i] = color
+
+    def show(self):
+        self._collections.show()
